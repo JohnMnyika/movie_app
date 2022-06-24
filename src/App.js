4aee14c7714cbf6c3a83570f9fcb0efbaadca047
JohnMnyika <jmnyika514@gmail.com>
2022-06-25 00:20:15 +0300

    setup the project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+//Api key = 1d9c4fe5
+
+const API_URL = 'http://www.omdbapi.com?apikey=1d9c4fe5';
+
+const App = () => {
+    return (
+        <h1>App</h1>
+    );
+}
+
+export default App;
+
